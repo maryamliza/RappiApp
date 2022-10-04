06e46c42a59a01f7438d5997c590c5d41256c893
MaryamLiza <maryam.liza@pucp.pe>
2022-10-04 17:14:43 -0500

    setup KOIN

--- app/src/main/java/com/kamiz/rappiapp/App.kt
+++ app/src/main/java/com/kamiz/rappiapp/App.kt
@@ -1,9 +1,17 @@
 package com.kamiz.rappiapp
 
 import android.app.Application
+import com.kamiz.rappiapp.di.repositoryModule
+import com.kamiz.rappiapp.di.viewModelsModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class App: Application() {
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(repositoryModule, viewModelsModule))
+        }
     }
 }
